799c56a242d14d32a6dd8acf458c1d0bdc607741
Devepre <bicycle2life@gmail.com>
2018-08-11 22:16:45 +0300

    Initial Commit

---
+++ Custom Menu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Custom Menu
+//
+//  Created by Limitation on 8/11/18.
+//  Copyright © 2018 Limitation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Custom Menu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Custom Menu
+//
+//  Created by Limitation on 8/11/18.
+//  Copyright © 2018 Limitation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
