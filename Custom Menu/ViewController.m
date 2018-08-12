dbc6a334cb044f91365b906aa69bddb3c4918932
Devepre <bicycle2life@gmail.com>
2018-08-12 19:17:24 +0300

    left gesture VC

--- Custom Menu/ViewController.m
+++ Custom Menu/ViewController.m
@@ -10,20 +10,54 @@
 #import "SKVCustomPresentationController.h"
 
 @interface ViewController ()
-
+@property (assign, nonatomic, getter=isViewControllerPresented) BOOL viewControllerPresented;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                                                                 action:@selector(handlePanGesture:)];
+    [self.view addGestureRecognizer:panGesture];
+}
+
+
+- (void)handlePanGesture:(UIPanGestureRecognizer *)recognizer {
+    static CGFloat translation = 0;
+    switch (recognizer.state) {
+        case UIGestureRecognizerStateBegan:
+            translation = 0;
+            break;
+        case UIGestureRecognizerStateChanged: {
+            translation += [recognizer translationInView:self.view].x;
+            CGFloat translationInPercents = translation / CGRectGetWidth(self.view.bounds);
+            if (!self.isViewControllerPresented && translationInPercents > 0.3) {
+                UIViewController *viewControllerToPresent = [self.storyboard instantiateViewControllerWithIdentifier:@"SecondViewController"];
+                [self presentViewController:viewControllerToPresent];
+                self.viewControllerPresented = YES;
+            }
+            break;
+        }
+        case UIGestureRecognizerStateEnded: {
+            self.viewControllerPresented = NO;
+            break;
+        }
+        default:
+            self.viewControllerPresented = NO;
+            break;
+    }
 }
 
 
 - (IBAction)showMenu:(UIButton *)sender {
     UIViewController *secondViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"SecondViewController"];
-    
+    [self presentViewController:secondViewController];
+}
+
+
+- (void)presentViewController:(UIViewController *)viewControllerToPresent {
     // For presentations which will use a custom presentation controller,
     // it is possible for that presentation controller to also be the
     // transitioningDelegate.  This avoids introducing another object
@@ -37,12 +71,12 @@
     SKVCustomPresentationController *presentationController NS_VALID_UNTIL_END_OF_SCOPE;
     
     presentationController = [[SKVCustomPresentationController alloc]
-                              initWithPresentedViewController:secondViewController
+                              initWithPresentedViewController:viewControllerToPresent
                               presentingViewController:self];
     
-    secondViewController.transitioningDelegate = presentationController;
+    viewControllerToPresent.transitioningDelegate = presentationController;
     
-    [self presentViewController:secondViewController
+    [self presentViewController:viewControllerToPresent
                        animated:YES
                      completion:nil];
 }
